606a9a1fd209994b1e8d4fe59099fcc2cfd1645f
Marco Polo <laurenlapoint@wifi_stu-10-38-77-51.uncc.edu>
2018-08-01 21:03:32 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,69 +31,69 @@ var data = [{
     wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
 
 },
-    {
-        question: "What does Phoebe change her name to in the final season?",
-        potentialAnswers: ["Princess Consuela Bananahammock", "Princess Pheffer Phefferman", "Kitty Kat", "Cindy Crawford"],
-        Answer: 0,
-        correctImage: "<img src='./assets/images/q3.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "What does Phoebe change her name to in the final season?",
+    potentialAnswers: ["Princess Consuela Bananahammock", "Princess Pheffer Phefferman", "Kitty Kat", "Cindy Crawford"],
+    Answer: 0,
+    correctImage: "<img src='./assets/images/q3.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "How many categories of towels does Monica have?",
-        potentialAnswers: [9, "11", 5, 13],
-        Answer: 1,
-        correctImage: "<img src='./assets/images/q4.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "How many categories of towels does Monica have?",
+    potentialAnswers: [9, "11", 5, 13],
+    Answer: 1,
+    correctImage: "<img src='./assets/images/q4.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "What is Chandler's dad's burlesque show called?",
-        potentialAnswers: ["Viva Las Gaygas", "It's Raining Men", "Vegas Gaygas", "One Lady Show"],
-        Answer: 0,
-        correctImage: "<img src='./assets/images/q5.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "What is Chandler's dad's burlesque show called?",
+    potentialAnswers: ["Viva Las Gaygas", "It's Raining Men", "Vegas Gaygas", "One Lady Show"],
+    Answer: 0,
+    correctImage: "<img src='./assets/images/q5.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "Which country does Chandler tell Janice he’s moving to?",
-        potentialAnswers: ["Turkey", "Brazil", "Yemen", "Bali"],
-        Answer: 2,
-        correctImage: "<img src='./assets/images/q6.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "Which country does Chandler tell Janice he’s moving to?",
+    potentialAnswers: ["Turkey", "Brazil", "Yemen", "Bali"],
+    Answer: 2,
+    correctImage: "<img src='./assets/images/q6.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "What is Monica’s biggest pet peeve?",
-        potentialAnswers: ["towels not folded", "unwashed dishes", "being late", "animals dressed as humans"],
-        Answer: 3,
-        correctImage: "<img src='./assets/images/q7.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "What is Monica’s biggest pet peeve?",
+    potentialAnswers: ["towels not folded", "unwashed dishes", "being late", "animals dressed as humans"],
+    Answer: 3,
+    correctImage: "<img src='./assets/images/q7.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "What is the word Ross uses to describe a relaxed mental state?",
-        potentialAnswers: ["sashimi", "nigiri", "sushi", "unagi"],
-        Answer: 3,
-        correctImage: "<img src='./assets/images/q8.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "What is the word Ross uses to describe a relaxed mental state?",
+    potentialAnswers: ["sashimi", "nigiri", "sushi", "unagi"],
+    Answer: 3,
+    correctImage: "<img src='./assets/images/q8.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "What fruit is Ross allergic to?",
-        potentialAnswers: ["watermelon", "oranges", "kiwi", "pineapple"],
-        Answer: 2,
-        correctImage: "<img src='./assets/images/q9.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "What fruit is Ross allergic to?",
+    potentialAnswers: ["watermelon", "oranges", "kiwi", "pineapple"],
+    Answer: 2,
+    correctImage: "<img src='./assets/images/q9.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
-    {
-        question: "What is the name of Joey's stuffed animal?",
-        potentialAnswers: ["Morice", "Stuwart", "George", "Robert"],
-        Answer: 0,
-        correctImage: "<img src='./assets/images/q10.gif'>",
-        wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
-    },
+{
+    question: "What is the name of Joey's stuffed animal?",
+    potentialAnswers: ["Morice", "Stuwart", "George", "Robert"],
+    Answer: 0,
+    correctImage: "<img src='./assets/images/q10.gif'>",
+    wrongImage: "<img class='wrongImage' src='./assets/images/thumbs_down.png'>",
+},
 
 ];
 
@@ -141,80 +141,6 @@ $(document).on("click", ".clickable", function () {
     userGuess = 0;
 
 
-
-    function renderAnswers(index) {
-    var liElements = " ";
-    for (var i = 0; i < data[index].potentialAnswers.length; i++) {
-        var res = data[index].potentialAnswers[i];
-        liElements += '<li data-id="' + res + '" class="clickable">' + res + '</li>';
-    }
-    console.log('Here are the elements', liElements);
-    return liElements;
-};
-
-//function that displays the question (question template)
-function displayQuestion(index) {
-    clearTimeout(postQuestionViewTimer);
-    if (!doWeKeepPlaying(index)) {
-        var questionTemplate = "<div class='parent'><h3>" + data[index].question + "</h3><ul>" + renderAnswers(index) + "</ul></div>";
-        questionTimer();
-    }
-
-    else {
-        // $(".container").empty();
-        $(".container").html("<p>Game over! Your results: " + "<p> Correct Answers: " + correctAnswers + "</p>" + "<p> Wrong Answers: " + wrongAnswers + "</p>" + "<p> Unanswered: " + unanswered + "<p>" +
-            "<button id='reset'>Play Again!</button>");
-
-        console.log($("#reset"));
-
-        //$("#reset").show();
-        $("#reset").on("click", function () {
-            console.log("i'm here")
-            $("#reset").hide();
-            $("#instructions").hide();
-            displayQuestion(startingIndex);
-
-            questionChangeTimer();
-
-        });
-    }
-
-    $('#root').html(questionTemplate);
-
-};
-
-//quality check, function to run through full index of questions
-function doWeKeepPlaying(index) {
-    return data.length === index
-
-}
-
-//each question will run a timer of 15 seconds
-function questionTimer() {
-    timeLeft = 3;
-    timer = setInterval(function () {
-        if (timeLeft === 1) {
-            clearInterval(timer);
-            questionChangeTimer();
-            $("#timer").html("Time's up!");
-
-        }
-        timeLeft--;
-        $("#timer").html("Time Remaining: " + timeLeft);
-    }, 1000);
-
-}
-
-//there will be 2 seconds between each question
-function questionChangeTimer() {
-    postQuestionViewTimer = setTimeout(function () {
-        clearInterval(timer);
-        startingIndex++;
-        displayQuestion(startingIndex);
-    }, 2000);
-
-};
-
 });
 
 //renderAnswer function creates an list of possible answers on the page. It loops through the index of the answer array.
@@ -240,12 +166,10 @@ function displayQuestion(index) {
         // $(".container").empty();
         $(".container").html("<p>Game over! Your results: " + "<p> Correct Answers: " + correctAnswers + "</p>" + "<p> Wrong Answers: " + wrongAnswers + "</p>" + "<p> Unanswered: " + unanswered + "<p>" +
             "<button id='reset'>Play Again!</button>");
-
-        console.log($("#reset"));
-
         //   $("#reset").show();
+
         $("#reset").on("click", function () {
-            console.log("i'm here")
+
             $("#reset").hide();
             $("#instructions").hide();
             displayQuestion(startingIndex);
@@ -253,6 +177,7 @@ function displayQuestion(index) {
             questionChangeTimer();
 
         });
+
     }
 
     $('#root').html(questionTemplate);
